cce7233880f32a7e62cd270c452bfa5d48a2ce45
hwyang <89916961+yhwoooo@users.noreply.github.com>
2024-06-12 23:45:03 +0900

    프로필 변경

--- config.js
+++ config.js
@@ -13,10 +13,10 @@ const siteConfig = {
 const users = [
   {
     id: 0, // default author
-    username: "licat",
+    username: "mura",
     company: "weniv",
     position: "CEO",
-    img: "img/user/profile-licat.png",
+    img: "img/user/profile-mura",
   },
 ];
 
